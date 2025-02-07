74157e9500108bbc50cde15473e7096df7898db7
Charles Effiong <charleseffiong28@gmail.com>
2025-02-07 11:53:54 +0100

    form

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ const username = document.getElementById('username').Value.trim()
 const email = document.getElementById('email').Value.trim()
 const password = document.getElementById('password').value.trim()
 
-
+const isValid = true;
+const messages = []
+if (username.value.length < 3) {
+    isValid = false;
+    return;
+} else {
+    messages.textContent = "Username is not long enough"
+}
+if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailInput.value)) {
+    isValid = false;
+    return;
+} else {
+    messages.textContent = "Invalid Email"
+}
+if (password.value.length < 8) {
+    isValid = false;
+} else{
+    messages.textContent = "Password is too Short"
+}
 })
