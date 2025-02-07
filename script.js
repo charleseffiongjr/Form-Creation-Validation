c8ff08cd13c18e59284326bfe6a4aaee16f8177b
Charles Effiong <charleseffiong28@gmail.com>
2025-02-07 11:32:36 +0100

    form

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
-})
+
+
 const form = document.getElementById('registration-form');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 });
 const feedbackDiv = getElementById('form-feedback')
+const username = document.getElementById('username').Value.trim()
+const email = document.getElementById('email').Value.trim()
+const password = document.getElementById('password').value.trim()
+
+
+})
