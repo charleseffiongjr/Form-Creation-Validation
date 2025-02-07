6d4a5cc262a275e82f8c62e0a708c525e959a737
Charles Effiong <charleseffiong28@gmail.com>
2025-02-07 23:26:12 +0100

    fetch-data

--- fetch-data.js
+++ fetch-data.js
@@ -5,6 +5,8 @@ async function fetchUserData () {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         const users = await response.json()
      }
-     catch('error')
+     catch(error){
+        
+     }
     
 }
