9bc9427ffae4352c0830b24cdfdcde8fcd48aabd
Charles Effiong <charleseffiong28@gmail.com>
2025-02-07 23:22:13 +0100

    fetch-data

--- fetch-data.js
+++ fetch-data.js
@@ -1,5 +1,9 @@
 async function fetchUserData () {
      const apiUrl = 'https://jsonplaceholder.typicode.com/users';
-     const dataContainer = document.getElementById("api-data")
+     const dataContainer = document.getElementById("api-data");
+     try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const users = await response.json('https://jsonplaceholder.typicode.com/users')
+     }
     
 }
